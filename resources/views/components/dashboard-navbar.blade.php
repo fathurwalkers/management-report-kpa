2c31a3ef3d6644d1aaf416b7a5396c72084bcc46
fathurwalkers <fathurwalkers44@gmail.com>
2024-09-28 10:18:47 +0700

    update

--- resources/views/components/dashboard-navbar.blade.php
+++ resources/views/components/dashboard-navbar.blade.php
@@ -3,38 +3,132 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <!-- Add icons to the links using the .nav-icon class
    with font-awesome or any other icon font library -->
-            <li class="nav-item menu-open">
-                <a href="#" class="nav-link active">
-                    <i class="nav-icon fas fa-tachometer-alt"></i>
-                    <p>
-                        Starter Pages
-                        <i class="right fas fa-angle-left"></i>
-                    </p>
-                </a>
-                <ul class="nav nav-treeview">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link active">
-                            <i class="far fa-circle nav-icon"></i>
-                            <p>Active Page</p>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">
-                            <i class="far fa-circle nav-icon"></i>
-                            <p>Inactive Page</p>
-                        </a>
-                    </li>
-                </ul>
-            </li>
             <li class="nav-item">
                 <a href="#" class="nav-link">
                     <i class="nav-icon fas fa-th"></i>
                     <p>
-                        Simple Link
-                        <span class="right badge badge-danger">New</span>
+                        Beranda
+                        {{-- <span class="right badge badge-danger">New</span> --}}
                     </p>
                 </a>
             </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-edit"></i>
+                    <p>
+                        Divisi
+                        <i class="fas fa-angle-left right"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview" style="display: none;">
+                    <li class="nav-item">
+                        <a href="pages/forms/general.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Human Resource Dept. </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/forms/advanced.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>IT </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/forms/editors.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Purchasing</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/forms/validation.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Admin</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/forms/validation.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Pengawas Tambang</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/forms/validation.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Finance</p>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-th"></i>
+                    <p>
+                        Tugas
+                        {{-- <span class="right badge badge-danger">New</span> --}}
+                    </p>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-edit"></i>
+                    <p>
+                        Laporan
+                        <i class="fas fa-angle-left right"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview" style="display: none;">
+                    <li class="nav-item">
+                        <a href="pages/forms/general.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Human Resource Dept. </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/forms/advanced.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>IT </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/forms/editors.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Purchasing</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/forms/validation.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Admin</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/forms/validation.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Pengawas Tambang</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/forms/validation.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Finance</p>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-th"></i>
+                    <p>
+                        Pengaduan
+                        {{-- <span class="right badge badge-danger">New</span> --}}
+                    </p>
+                </a>
+            </li>
+
         </ul>
     </nav>
 </div>
--- resources/views/layouts/dashboard-layout.blade.php
+++ resources/views/layouts/dashboard-layout.blade.php
@@ -13,6 +13,11 @@
     <link rel="stylesheet" href="{{ asset('assets/adminlte3') }}/plugins/fontawesome-free/css/all.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('assets/adminlte3') }}/dist/css/adminlte.min.css">
+    <style>
+        .brand-image {
+            filter: drop-shadow(1px 1px 1px white) drop-shadow(-1px 1px 1px white) drop-shadow(1px -1px 1px white) drop-shadow(-1px -1px 1px rgb(255, 255, 255));
+        }
+    </style>
     @stack('css')
 </head>
 
@@ -28,10 +33,7 @@
                             class="fas fa-bars"></i></a>
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
-                    <a href="index3.html" class="nav-link">Pengaduan</a>
-                </li>
-                <li class="nav-item d-none d-sm-inline-block">
-                    <a href="#" class="nav-link">Informasi</a>
+                    <a href="#" class="nav-link text-dark">Informasi</a>
                 </li>
             </ul>
 
@@ -51,7 +53,7 @@
             <!-- Right navbar links -->
             <ul class="navbar-nav ml-auto">
                 <!-- Messages Dropdown Menu -->
-                <li class="nav-item dropdown">
+                {{-- <li class="nav-item dropdown">
                     <a class="nav-link" data-toggle="dropdown" href="#">
                         <i class="far fa-comments"></i>
                         <span class="badge badge-danger navbar-badge">3</span>
@@ -111,7 +113,7 @@
                         <div class="dropdown-divider"></div>
                         <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
                     </div>
-                </li>
+                </li> --}}
                 <!-- Notifications Dropdown Menu -->
                 <li class="nav-item dropdown">
                     <a class="nav-link" data-toggle="dropdown" href="#">
@@ -145,8 +147,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#"
-                        role="button">
+                    <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
                         <i class="fas fa-th-large"></i>
                     </a>
                 </li>
@@ -157,11 +158,17 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
-                <img src="{{ asset('assets/adminlte3') }}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
-                    class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
-            </a>
+            {{-- <a href="index3.html" class="brand-link"> --}}
+            {{-- <img src="{{ asset('assets/img') }}/logo-eco.png" alt="ECOASPHALT" class="brand-image"
+                    style="opacity: .8"> --}}
+            {{-- <span class="brand-text font-weight-light">PT. KPA</span> --}}
+            {{-- </a> --}}
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-12 d-flex justify-content-center">
+                    <img src="{{ asset('assets/img') }}/logo-eco.png" alt="ECOASPHALT" class="brand-image my-2"
+                        style="" width="200px">
+                </div>
+            </div>
 
             <!-- Sidebar -->
             <div class="sidebar">
@@ -178,88 +185,13 @@
         <div class="content-wrapper">
             <!-- Content Header (Page header) -->
             <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0">Starter Page</h1>
-                        </div><!-- /.col -->
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active">Starter Page</li>
-                            </ol>
-                        </div><!-- /.col -->
-                    </div><!-- /.row -->
-                </div><!-- /.container-fluid -->
+                @yield('content-header')
             </div>
             <!-- /.content-header -->
 
             <!-- Main content -->
             <div class="content">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-
-                                    <p class="card-text">
-                                        Some quick example text to build on the card title and make up the bulk of the
-                                        card's
-                                        content.
-                                    </p>
-
-                                    <a href="#" class="card-link">Card link</a>
-                                    <a href="#" class="card-link">Another link</a>
-                                </div>
-                            </div>
-
-                            <div class="card card-primary card-outline">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-
-                                    <p class="card-text">
-                                        Some quick example text to build on the card title and make up the bulk of the
-                                        card's
-                                        content.
-                                    </p>
-                                    <a href="#" class="card-link">Card link</a>
-                                    <a href="#" class="card-link">Another link</a>
-                                </div>
-                            </div><!-- /.card -->
-                        </div>
-                        <!-- /.col-md-6 -->
-                        <div class="col-lg-6">
-                            <div class="card">
-                                <div class="card-header">
-                                    <h5 class="m-0">Featured</h5>
-                                </div>
-                                <div class="card-body">
-                                    <h6 class="card-title">Special title treatment</h6>
-
-                                    <p class="card-text">With supporting text below as a natural lead-in to additional
-                                        content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-
-                            <div class="card card-primary card-outline">
-                                <div class="card-header">
-                                    <h5 class="m-0">Featured</h5>
-                                </div>
-                                <div class="card-body">
-                                    <h6 class="card-title">Special title treatment</h6>
-
-                                    <p class="card-text">With supporting text below as a natural lead-in to additional
-                                        content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- /.col-md-6 -->
-                    </div>
-                    <!-- /.row -->
-                </div><!-- /.container-fluid -->
+                @yield('main-content')
             </div>
             <!-- /.content -->
         </div>
